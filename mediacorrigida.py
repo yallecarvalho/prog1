fafc474c19f33e7097dd5b2a020ca42fe0f84417
yallecarvalho <54670687+yallecarvalho@users.noreply.github.com>
2019-09-02 10:29:55 -0300

    Add files via upload

---
+++ mediacorrigida.py
@@ -0,0 +1,12 @@
+# UFCG - P1 - 2019.2
+# Yalle Carvalho - 119210523
+# Corrige Média
+
+nota1 = 10
+nota2 = 5
+
+media = (nota1 + nota2) /2
+
+print("Nota 1: %4.1f" % nota1)
+print("Nota 2: %4.1f" % nota2)
+print("Média : %4.1f" % media)
