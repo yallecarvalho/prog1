4aa9b68285ecfbc6610ad5e0c402ec4b37f398f8
yallecarvalho <54670687+yallecarvalho@users.noreply.github.com>
2019-09-02 10:28:45 -0300

    Add files via upload

---
+++ desvio.py
@@ -0,0 +1,13 @@
+# UFCG - P1 - 2019.2
+# Yalle Carvalho - 119210523
+# Desvio 
+
+a = float(input())
+b = float(input())
+c = float(input())
+n = float(input())
+
+media = (a + b + c) / 3
+desvio = n - media 
+
+print('{:.1f}'.format(desvio))
