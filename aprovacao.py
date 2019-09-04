cf3748960059ffdc0c50918f9db11741b563f88e
yallecarvalho <54670687+yallecarvalho@users.noreply.github.com>
2019-09-04 19:45:11 -0300

    Add files via upload

---
+++ aprovacao.py
@@ -0,0 +1,10 @@
+# UFCG - P1 - 2019.2
+# Yalle Carvalho - 119210523
+# Aprovação Unidades
+
+unidade = int(input('Unidade? '))
+media = float(input('Média de aprovação na unidade? '))
+
+proxima_unidade = (unidade + 1)
+
+print("\nO aluno vai para a unidade {0} com média {1:.1f}.".format(proxima_unidade, media))
