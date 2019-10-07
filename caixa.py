d9ace4cc9aeab6b3ca4ed36a9f0b023b3e7a9da1
yallecarvalho <54670687+yallecarvalho@users.noreply.github.com>
2019-10-07 10:54:00 -0300

    Update caixa.py

--- caixa.py
+++ caixa.py
@@ -6,16 +6,22 @@ a = False
 for i in range(n):
     dados = input()
     d = dados.split()
-    num_dados += 1
     if not a and int(d[0]) < 0:
         a = True
         print('dado inconsistente. peso negativo.')
-    elif not a and int(d[1]) < 0:
+    elif not a:
+        num_dados += 1
+    if not a and int(d[1]) < 0:
         a = True
         print('dado inconsistente. combustível negativo.')
-    elif not a and int(d[2]) < 0:
+    elif not a:
+        num_dados += 1
+    if not a and int(d[2]) < 0:
         a = True
         print('dado inconsistente. altitude negativa.')
+    elif not a:
+        num_dados += 1
 
 print('{} dados válidos.'.format(num_dados))
+
 
