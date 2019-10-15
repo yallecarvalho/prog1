14e4901cba3f99c036634536413957cc86dcb7cc
yallecarvalho <54670687+yallecarvalho@users.noreply.github.com>
2019-10-15 20:23:55 -0300

    Add files via upload

---
+++ grep.py
@@ -0,0 +1,213 @@
+# UFCG - Programação 1
+# Yalle Carvalho
+# Grep
+
+palavra = input()
+num_frases = int(input())
+
+pertencente = False
+
+for i in range(num_frases):
+    frases = input()
+    palavras = frases.split()
+
+    if not pertencente and palavras[i] == palavra:
+        pertencente = True
+        print(frases)# UFCG - Programação 1
+        # Yalle Carvalho
+        # Grep
+
+        palavra = input()
+        num_frases = int(input())
+        
+        pertencente = False
+
+        for i in range(num_frases):
+            frases = input()
+            palavras = frases.split()
+
+      
+  
+    if not pertencente and palavras[i] == palavra:
+        pertencente = True
+        print(frases)# UFCG - Programação 1
+        # Yalle Carvalho
+        # Grep
+
+        palavra = input()
+        num_frases = int(input())
+        
+        pertencente = False
+
+        for i in range(num_frases):
+            frases = input()
+            palavras = frases.split()
+
+      
+  
+    if not pertencente and palavras[i] == palavra:
+        pertencente = True
+        print(frases)# UFCG - Programação 1
+        # Yalle Carvalho
+        # Grep
+
+        palavra = input()
+        num_frases = int(input())
+        
+        pertencente = False
+
+        for i in range(num_frases):
+            frases = input()
+            palavras = frases.split()
+
+      
+  
+    if not pertencente and palavras[i] == palavra:
+        pertencente = True
+        print(frases)# UFCG - Programação 1
+        # Yalle Carvalho
+        # Grep
+
+        palavra = input()
+        num_frases = int(input())
+        
+        pertencente = False
+
+        for i in range(num_frases):
+            frases = input()
+            palavras = frases.split()
+
+      
+  
+    if not pertencente and palavras[i] == palavra:
+        pertencente = True
+        print(frases)# UFCG - Programação 1
+        # Yalle Carvalho
+        # Grep
+
+        palavra = input()
+        num_frases = int(input())
+        
+        pertencente = False
+
+        for i in range(num_frases):
+            frases = input()
+            palavras = frases.split()
+
+      
+  
+    if not pertencente and palavras[i] == palavra:
+        pertencente = True
+        print(frases)# UFCG - Programação 1
+        # Yalle Carvalho
+        # Grep
+
+        palavra = input()
+        num_frases = int(input())
+        
+        pertencente = False
+
+        for i in range(num_frases):
+            frases = input()
+            palavras = frases.split()
+
+      
+  
+    if not pertencente and palavras[i] == palavra:
+        pertencente = True
+        print(frases)# UFCG - Programação 1
+        # Yalle Carvalho
+        # Grep
+
+        palavra = input()
+        num_frases = int(input())
+        
+        pertencente = False
+
+        for i in range(num_frases):
+            frases = input()
+            palavras = frases.split()
+
+      
+  
+    if not pertencente and palavras[i] == palavra:
+        pertencente = True
+        print(frases)# UFCG - Programação 1
+        # Yalle Carvalho
+        # Grep
+
+        palavra = input()
+        num_frases = int(input())
+        
+        pertencente = False
+
+        for i in range(num_frases):
+            frases = input()
+            palavras = frases.split()
+
+      
+  
+    if not pertencente and palavras[i] == palavra:
+        pertencente = True
+        print(frases)# UFCG - Programação 1
+        # Yalle Carvalho
+        # Grep
+
+        palavra = input()
+        num_frases = int(input())
+        
+        pertencente = False
+
+        for i in range(num_frases):
+            frases = input()
+            palavras = frases.split()
+
+      
+  
+    if not pertencente and palavras[i] == palavra:
+        pertencente = True
+        print(frases)# UFCG - Programação 1
+        # Yalle Carvalho
+        # Grep
+
+        palavra = input()
+        num_frases = int(input())
+        
+        pertencente = False
+
+        for i in range(num_frases):
+            frases = input()
+            palavras = frases.split()
+
+      
+  
+    if not pertencente and palavras[i] == palavra:
+        pertencente = True
+        print(frases)# UFCG - Programação 1
+        # Yalle Carvalho
+        # Grep
+
+        palavra = input()
+        num_frases = int(input())
+        
+        pertencente = False
+
+        for i in range(num_frases):
+            frases = input()
+            palavras = frases.split()
+# UFCG - Programação 1
+# Yalle Carvalho
+# Grep
+
+palavra = input()
+num_frases = int(input())
+        
+pertencente = False
+
+for i in range(num_frases):
+    frases = input()
+    palavras = frases.split()
+
+    if not pertencente and palavras[i] == palavra:
+        pertencente = True
+        print(frases)
