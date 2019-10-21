22f30a128aebd728115476e90afa1a4dec9dc7d4
yallecarvalho <54670687+yallecarvalho@users.noreply.github.com>
2019-10-21 15:22:08 -0300

    Add files via upload

---
+++ encontra.py
@@ -0,0 +1,18 @@
+# UFCG - Programação 1
+# Yalle Carvalho
+# Encontra elemento
+
+# função para encontrar
+def meu_in(sequencia, num):
+    for e in sequencia:
+        if e == num:
+            return True
+    return False
+
+num = input()
+sequencia = input()
+
+if meu_in(sequencia, num):
+    print('sim')
+else:
+    print('não')
